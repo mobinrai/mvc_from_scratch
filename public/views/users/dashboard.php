6a1a21b04184366d754da15a461b12b5a8265592
mobinrai <mobinraee@gmail.com>
2022-11-22 12:33:16 +0100

    change dashboard style

--- public/views/users/dashboard.php
+++ public/views/users/dashboard.php
@@ -1,1 +1,9 @@
-<h3>Welcome to user's dashboard</h3>
+<div id="latest-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Welcome to dashboard..</h3>
+            </div>
+        </div>
+    </div>
+</div>    
